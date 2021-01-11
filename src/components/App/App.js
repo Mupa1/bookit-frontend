81564aed5cef31708c72a96d8ba399d9398d99ee
Mupa1 <mupasmail@gmail.com>
2021-01-12 00:13:21 +0100

    remove styles from index and add page routes to app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HomePage from '../../pages/HomePage/HomePage';
+import SignIn from '../../pages/SignIn/SignIn';
+import SignUp from '../../pages/SignUp/SignUp';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={HomePage} />
+      <Route path="/signin" component={SignIn} />
+      <Route path="/signup" component={SignUp} />
     </Switch>
   </BrowserRouter>
 );
