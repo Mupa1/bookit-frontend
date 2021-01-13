dcf7a2e56e84b1720cc4a777e77de6cc177fcae5
Mupa1 <mupasmail@gmail.com>
2021-01-13 10:38:18 +0100

    add sign in logic

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,37 +1,88 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import styles from '../../styles/Auth.module.css';
+import { fetchUser } from '../../Redux/actions/index';
 
-const SignIn = () => (
-  <section className="container">
-    <div className={styles.homeLink}>
-      <Link to="/">Home</Link>
-    </div>
-    <div className={`${styles.formContainer} text-center`}>
-      <form data-testid="form">
-        <h3 className="text-center">Sign In</h3>
-        <div className="form-group">
-          <label htmlFor="email">
-            Email
-            <input type="email" name="email" id="email" className="form-control" required />
-          </label>
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">
-            Password
-            <input type="password" name="password" id="password" className="form-control" required />
-          </label>
-        </div>
-        <button type="submit" className="p-0 m-0">Submit</button>
-        <div>
-          Don&apos;t have an account.
-          {' '}
-          <Link to="/signup">Register here.</Link>
-        </div>
-      </form>
-    </div>
-  </section>
-);
+const SignIn = ({ user, fetchUser, history }) => {
+  const userObj = {
+    sign_in: {},
+  };
 
-export default SignIn;
+  const handleSignIn = () => {
+    history.push('/doctors');
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    await fetch('https://bookit-doc-appointments-api.herokuapp.com/api/v1/sign_in', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userObj),
+    })
+      .then(res => res.json())
+      .then(data => {
+        fetchUser(data.data.user);
+        localStorage.setItem('token', data.data.user.authentication_token);
+        if (Object.keys(user).length > 0) {
+          handleSignIn();
+        }
+      })
+      .catch(err => err);
+  };
+
+  const handleChange = e => {
+    userObj.sign_in = Object.assign(userObj.sign_in, {
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <section className="container">
+      <div className={styles.homeLink}>
+        <Link to="/">Home</Link>
+      </div>
+      <div className={`${styles.formContainer} text-center`}>
+        <form data-testid="form" onSubmit={handleSubmit}>
+          <h3 data-testid="title" className="text-center">Sign In</h3>
+          <div className="form-group">
+            <label htmlFor="email">
+              Email
+              <input type="email" name="email" id="email" className="form-control" required onChange={handleChange} />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">
+              Password
+              <input type="password" name="password" id="password" className="form-control" required onChange={handleChange} />
+            </label>
+          </div>
+          <button type="submit" className="p-0 m-0">Submit</button>
+          <div>
+            Don&apos;t have an account.
+            {' '}
+            <Link to="/SignIn">Register here.</Link>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+const mapStateToProps = state => ({ user: state.user });
+
+const mapDispatchToProps = dispatch => ({
+  fetchUser: value => dispatch(fetchUser(value)),
+});
+
+SignIn.propTypes = {
+  fetchUser: PropTypes.func.isRequired,
+  user: PropTypes.instanceOf(Object).isRequired,
+  history: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
--- src/components/SignIn/SignIn.test.js
+++ src/components/SignIn/SignIn.test.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import SignIn from './SignIn';
+import SignUp from './SignIn';
+import store from '../../Redux/store';
 
-describe('SignIn', () => {
-  it('renders SignIn component with a title', () => {
-    render(
-      <Router>
-        <SignIn />
-      </Router>,
+describe('SignUp', () => {
+  it('renders SignUp component with a title', () => {
+    const div = document.createElement('div');
+    const { queryAllByTestId } = render(
+      <Provider store={store}>
+        <Router>
+          <Route path="/SignUp" component={SignUp} />
+        </Router>
+      </Provider>, div,
     );
-    expect(screen.getByText('Sign In')).toBeInTheDocument();
+    const title = queryAllByTestId('title');
+    expect(title).toBeTruthy();
   });
 
   it('should have a Sign In form', () => {
+    const div = document.createElement('div');
     const { queryAllByTestId } = render(
-      <Router>
-        <SignIn />
-      </Router>,
+      <Provider store={store}>
+        <Router>
+          <Route path="/SignUp" component={SignUp} />
+        </Router>
+      </Provider>, div,
     );
     const form = queryAllByTestId('form');
     expect(form).toBeTruthy();
